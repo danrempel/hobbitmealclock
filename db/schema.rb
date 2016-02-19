f7f20045003779bb1a83c570850a5924452ee31d
Dan Rempel <dan.rempel0@gmail.com>
2016-02-19 16:39:48 -0700

    Added a model for Responses

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160219233107) do
+ActiveRecord::Schema.define(version: 20160219233539) do
 
   create_table "bots", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "responses", force: :cascade do |t|
+    t.string   "message"
+    t.string   "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
