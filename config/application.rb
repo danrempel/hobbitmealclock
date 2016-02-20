cf921716422c26fdfb40405a902a8f83037ebc18
Dan Rempel <dan.rempel0@gmail.com>
2016-02-19 20:25:26 -0700

    Set config.time_zone in application.rb for cron jobs

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Hobbitmealclock
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Set time zone for cron jobs
+    config.time_zone = "Mountain Time (US & Canada)" => "America/Denver"
   end
 end
