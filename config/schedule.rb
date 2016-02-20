4b74a6cf94d82d85c81856d96a2d9417f9442378
Dan Rempel <dan.rempel0@gmail.com>
2016-02-19 20:17:31 -0700

    Fixed a typo in schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,18 +8,24 @@ end
 
 every 1.day, :at => '9:00 am' do
   runner "Bot.second_breakfast"
+end
 
 every 1.day, :at => '11:00 am' do
   runner "Bot.elevenses"
+end
 
 every 1.day, :at => '1:00 pm' do
   runner "Bot.lunch"
+end
 
 every 1.day, :at => '3:00 pm' do
   runner "Bot.afternoon_tea"
+end
 
 every 1.day, :at => '6:00 pm' do
  runner "Bot.dinner"
+end
 
 every 1.day, :at => '9:00 pm' do
   runner "Bot.supper"
+end
